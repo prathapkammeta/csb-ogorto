598374117d2d22ac36b1dce48910f9e304c16115
Code Sandbox <hello@codesandbox.io>
2022-08-31 09:07:17 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.0ae8b146.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdiv-color-change"]=this["webpackJsonpdiv-color-change"]||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c(3),r=(c(12),c(0));function i(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"Hello CodeSandbox"}),Object(r.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var s=document.getElementById("root");Object(o.createRoot)(s).render(Object(r.jsx)(n.StrictMode,{children:Object(r.jsx)(i,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.0ae8b146.chunk.js.map
